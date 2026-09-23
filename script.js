8d7432bb61008a5d7eed620ebd1a38755333a41a
Vasu <vasu15573@gmail.com>
2026-09-23 12:08:12 +0530

    Update login message on main

--- script.js
+++ script.js
@@ -21,6 +21,6 @@ function login() {
     if (username === "") {
         message.textContent = "Please enter your username.";
     } else {
-        message.textContent = "Login successful!";
+        message.textContent = "Login successful! You can start the exam.";
     }
 }
